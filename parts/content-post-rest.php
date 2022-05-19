ed61016b35866b177b43f8e64cfb80d87b09c350
accrane <austincrane@gmail.com>
2022-05-19 15:29:05 -0400

    fix

--- parts/content-post-rest.php
+++ parts/content-post-rest.php
@@ -40,7 +40,7 @@ if( $catId == '23' ) {
 					<div class="textcol">
 						<div class="inside fadeIn wow" data-wow-delay="<?php echo $sec;?>s">
 							<div class="wrap">
-								<h2 class="title"><?php echo $title . ' ' . $i; ?></h2>
+								<h2 class="title"><?php echo $title; ?></h2>
 								<?php $postdate = get_the_date('M j, Y'); ?>
 								<div class="store-postdate"><span><?php echo $postdate ?></span></div>
 

f1aa57f77d0044c1a1f50770625480f6067547ef
accrane <austincrane@gmail.com>
2023-05-11 12:15:30 -0400

    add retreats

--- inc/post-types.php
+++ inc/post-types.php
@@ -64,6 +64,14 @@ function js_custom_init() {
             'menu_icon' => 'dashicons-location-alt',
             'supports'  => array('title','editor')
         ),
+        array(
+            'post_type' => 'retreat',
+            'menu_name' => 'Retreats',
+            'plural'    => 'Retreats',
+            'single'    => 'Retreat',
+            'menu_icon' => 'dashicons-book-alt',
+            'supports'  => array('title','editor')
+        ),
         // array(
         //     'post_type' => 'film-series',
         //     'menu_name' => 'Film Series',
